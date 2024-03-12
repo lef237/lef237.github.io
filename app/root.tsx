b30484ba4eb0e1feebe6b2f09a8fb3c60f7b1211
lef237 <lef237k5@gmail.com>
2024-03-12 19:49:55 +0900

    Make Tailwind CSS work in Remix SPA stable

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,8 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import "./tailwind.css";
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
